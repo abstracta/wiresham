cdb66a330911df811bc70ede6fcbf8b233917eea
Roger Abelenda <roger.abelenda@abstracta.com.uy>
2018-06-06 17:34:34 -0300

    Remove pom.xml development version update from release script
    To keep the project simple and avoid adding extra complexity to setup git repository to allow commit the version and additional steps in deploy script, we have decided to just not maintain the version in the pom.xml.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,12 +7,7 @@
 
 set -eo pipefail
 
-VERSION=${TRAVIS_TAG:1}
 echo $GPG_SECRET_KEYS | base64 --decode | $GPG_EXECUTABLE --import
 echo $GPG_OWNERTRUST | base64 --decode | $GPG_EXECUTABLE --import-ownertrust
-mvn --batch-mode versions:set -DnewVersion=$VERSION --settings .travis/settings.xml
+mvn --batch-mode versions:set -DnewVersion=${TRAVIS_TAG:1} --settings .travis/settings.xml
 mvn --batch-mode clean deploy -Prelease -DskipTests=true --settings .travis/settings.xml
-#we need this due to https://github.com/mojohaus/versions-maven-plugin/issues/207
-mvn --batch-mode versions:set -DnextSnapshot=true -DnewVersion=${VERSION}-SNAPSHOT
-mvn --batch-mode versions:set -DnextSnapshot=true
-mvn --batch-mode scm:checkin -Dmessage="[skip ci] Update version to next development version"
