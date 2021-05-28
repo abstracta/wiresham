7914b60163e9d0c4befe8998d777f769ba1bf49d
Roger Abelenda <roger.abelenda@abstracta.com.uy>
2021-05-28 18:33:58 -0300

    Remove pending references to Travis

--- .github/maven-central-deploy.sh
+++ .github/maven-central-deploy.sh
@@ -3,7 +3,7 @@
 # This script takes care of deploying tagged versions to maven central and updating the pom.xml
 # version with next development version.
 #
-# Required environment variables: TRAVIS_TAG, GPG_SECRET_KEYS, GPG_OWNERTRUST, GPG_EXECUTABLE
+# Required environment variables: GPG_SECRET_KEYS, GPG_OWNERTRUST, GPG_EXECUTABLE
 
 set -eo pipefail
 
