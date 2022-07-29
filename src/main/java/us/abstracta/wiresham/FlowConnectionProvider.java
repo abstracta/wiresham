1e3d202c6d01eb692566e7467760e62f3258ea41
Joaquin Araujo <joaquin.araujo@abstracta.com.uy>
2022-07-29 17:02:02 -0300

    Format code to comply contribution standards

--- src/main/java/us/abstracta/wiresham/FlowConnectionProvider.java
+++ src/main/java/us/abstracta/wiresham/FlowConnectionProvider.java
@@ -5,14 +5,17 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public interface FlowConnectionProvider {
-  
+
   FlowConnection get(int port) throws ExecutionException, InterruptedException, IOException;
 
   void init(List<Integer> ports, FlowConnection flowConnection);
 
-  default void assignFlowConnection(int port, FlowConnection flowConnection) {};
+  default void assignFlowConnection(int port, FlowConnection flowConnection) {
+  }
 
-  default boolean requiresFlowConnection(int port) {return false;};
+  default boolean requiresFlowConnection(int port) {
+    return false;
+  }
 
   void closeConnections() throws IOException;
 }
